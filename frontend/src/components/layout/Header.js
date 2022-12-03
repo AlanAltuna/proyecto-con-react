331f7efddc77512ae898a032588bffcc6f2682a0
Alan Altuna <altuna39@gmail.com>
2022-12-03 02:42:15 -0300

    nav responsive ok

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -8,15 +8,10 @@ const Header = (props) => {
            <img src="logo.png" width="80px" height="80px" alt="logo" className="logoHeader"/>
            <a href="#s1">InfoVapes</a>
         </div>
-        <div class="button" >
-        <button class="ham" type="button">
-    <span class="bar-1"></span>
-    <span class="bar-2"></span>
-    <span class="bar-3"></span>
-</button>
-</div>
         </header>
     )
 }
+
+
 
 export default Header;
--- frontend/src/components/layout/Nav.js
+++ frontend/src/components/layout/Nav.js
@@ -1,17 +1,25 @@
 import {NavLink} from "react-router-dom";
-import React from "react";
+import {useState} from "react";
 import "../../styles/layout/Nav.css"
 import "../../styles/layout/Header.css"
+import BurguerButton from "../button.jsx"
 
-const button = document.querySelector('.button');
-const enlaces = document.querySelector('navBar');
 
 
 
 const Nav = (props) => {
+
+    const [clicked, setClicked] = useState(false);
+  const handleClick = () => {
+    //cuando esta true lo pasa a false y vice versa
+    setClicked(!clicked)
+  }
     return (
         <nav>
-<div className="navBar">
+            <div className='burguer'>
+          <BurguerButton clicked={clicked} handleClick={handleClick} />
+        </div>
+<div className={`navBar ${clicked ? 'active' : ''}`}>
 <ul>
 <li><NavLink to="/" className={({isActive}) => isActive ? "Activo" : undefined }>INICIO</NavLink></li>
 <li><NavLink to="/Vapes" className={({isActive}) => isActive ? "Activo" : undefined }>VAPES</NavLink></li>
