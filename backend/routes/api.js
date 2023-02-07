b4922e84fab7afb6924a883b2cf9787fe2b92e22
Alan Altuna <altuna39@gmail.com>
2023-02-07 13:03:05 -0300

    envio de formulario

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var novedadesModel = require('./../models/novedadesModel');
 var cloudinary = require('cloudinary').v2; 
+var nodemailer = require('nodemailer');
 
 router.get('/novedades', async function (req, res, next) {
     let novedades = await novedadesModel.getNovedades();
@@ -26,6 +27,28 @@ router.get('/novedades', async function (req, res, next) {
     })
 
     res.json(novedades);
+});
+
+router.post('/Contacto', async (req, res) => {
+    const mail = {
+        to: 'altuna39@gmail.com',
+        subject: 'Contacto web',
+        html: `${req.body.nombre} se contacto a traves de infoVapes y quiere más información a este correo: ${req.body.email} <br> además, hizo el siguiente comentario: ${req.body.mensaje} <br> su tel es: ${req.body.telefono}`   
+    }
+    const transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    });
+    await transport.sendMail(mail)
+
+    res.status(201).json({
+        error: false,
+        message: 'Mensaje enviado'
+    });
 });
 
 module.exports = router;
--- frontend/src/pages/ContactoPage.js
+++ frontend/src/pages/ContactoPage.js
@@ -1,16 +1,62 @@
-import React from "react";
-import "../styles/pages/ContactoPage.css"
+import React, { useState } from 'react';
+import "../styles/pages/ContactoPage.css";
+import axios from "axios";
+
+
 const Contacto = (props) => {
+
+const initialForm = {
+    nombre: '',
+    email: '',
+    telefono: '',
+    mensaje: ''
+}
+
+const [sending, setSending] = useState(false);
+const [msg, setMsg] = useState('');
+const [formData, setFormData] = useState(initialForm);
+
+const handleChange = e => {
+    const {name, value} = e.target;
+    setFormData(oldData => ({
+        ...oldData,
+        [name]: value
+    }));
+}
+
+const handleSubmit = async e => {
+    e.preventDefault();
+    setMsg('');
+    setSending(true)
+    const response= await
+    axios.post('http://localhost:3000/api/Contacto', formData);
+    setMsg (response.data.message);
+    if (response.data.error === false) {
+        setFormData(initialForm)
+    }
+}
+
+
+
+
     return (
         <div className="formulario">
             <img src="foto3.jpg" alt="portada4" width="100%" height="100%" className="fotoPortada"/>
-    <form className="form1">
-        <label><input type="name" placeholder="NOMBRE" class="fdos" required/></label>
-        <label><input type="email" placeholder="E-MAIL" class="fdos" required/></label>
-        <label><input  placeholder="ASUNTO" class="fdos" required/></label>
-        <label><input type="text"  class="ftres" required/></label>
-        <label><input type="submit" class="enviar"/></label>
+
+    <form className="form1" action="/Contacto" method="post" onSubmit={handleSubmit}>
+      
+        <input type="text" placeholder="NOMBRE" class="fdos" name="nombre" value={formData.nombre} onChange={handleChange}/>
+       
+        <input type="text" placeholder="E-MAIL" class="fdos" name="email" value={formData.email} onChange={handleChange}/>
+       
+        <input type="text" placeholder="TELEFONO" class="fdos" name="telefono" value={formData.telefono} onChange={handleChange}/>
+        
+        <textarea class="ftres" name="mensaje" value={formData.mensaje} onChange={handleChange}></textarea>
+       
+        <input type="submit" class="enviar" value="Enviar"/>
     </form>
+    {sending ? <p>Enviando..</p> : null}
+        {msg ? <p>{msg}</p> : null}
 </div>
 
     )
